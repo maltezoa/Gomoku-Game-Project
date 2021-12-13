705f0950793c620db050a588fe466c5b9b9dee52
Jonathan Bradford <jtbradford@mail.fresnostate.edu>
2021-12-12 16:10:13 -0800

    added my CSS folder + images + started commenting

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <!-- Insert CSS File for Index Menu here-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="CSS/offlineGame.css">
     </head>
     <body>
         <script>
